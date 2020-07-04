374d7d40b2b98fec25b64aea1a371aba06a1e73a
ASiracusa <fedorafilmz@gmail.com>
2020-07-04 17:10:45 -0400

    Bootstrap Setup

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <span className="badge badge-primary">Something</span>
+          <table class="table table-dark">
+            <tr>
+              <th>Category 1</th>
+              <th>Category 2</th>
+              <th>Category 3</th>
+            </tr>
+            <tr>
+              <td>Thing 1</td>
+              <td>Thing 2</td>
+              <td>Thing 3</td>
+            </tr>
+            <tr>
+              <td>Thing 4</td>
+              <td>Thing 5</td>
+              <td>Thing 6</td>
+            </tr>
+          </table>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
