aa12e1c35174bcc8b45a9c1e9d26787b9d8b7bf2
ASiracusa <fedorafilmz@gmail.com>
2021-03-20 22:57:14 -0400

    Restructured Upcoming as Function, Fixed Offset Index

--- scheduler/src/components/daybox.jsx
+++ scheduler/src/components/daybox.jsx
@@ -1,17 +1,36 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-class DayBox extends Component {
-  state = {};
+const order = [1, 2, 3];
 
-  render() {
-    return (
-      <React.Fragment>
-        <td className="day-cell">
-          <div className={this.props.isCurrentMonth}>{this.props.day}</div>
-        </td>
-      </React.Fragment>
-    );
-  }
+function DayBox(props) {
+  const [oldOrder, newOrder] = useState(order);
+  
+  return (
+    <td className="day-cell" key={props.day}>
+      <div className={"calendar-space" + ((props.date.getDate() === props.day) ? " calendar-space-today" : "")}>
+        {props.day}
+        <button className="add-card-button"> + </button>
+        <div>
+          <Droppable droppableId={"calendar-space-content-" + props.day + "-" + props.weekday}>
+              {(provided) => (
+                  <ul className={"calendar-space-content" + props.day + "-" + props.weekday} {...provided.droppableProps} ref={provided.innerRef}>
+                      {oldOrder.map((ind) => {
+                          return (<Draggable key={"" + props.day + "-" + ind} draggableId={"" + props.day + "-" + ind} index={ind - 1}>
+                              {(provided) => (
+                                  <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                      item {ind}
+                                  </li>
+                              )}
+                          </Draggable>);
+                      })}
+                  </ul>
+              )}
+          </Droppable>
+        </div>
+      </div>
+    </td>
+  );
 }
 
 export default DayBox;
--- scheduler/src/components/upcoming.jsx
+++ scheduler/src/components/upcoming.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const MONTHS = [
@@ -16,50 +16,37 @@ const MONTHS = [
     "December",
 ];
 const MONTHLENGTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const order = [1, 2, 3];
 
-class Upcoming extends Component {
+function Upcoming () {
+    const [oldOrder, newOrder] = useState(order);
+    
+    return (
+        <React.Fragment>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+            <div className="calendar-header">:)</div>
+                <div className="upcoming-body">
+                    <table className="upcoming" cellPadding="0">
+                        <tbody>
+                            {generateUpcomingDays()}
+                        </tbody>
+                    </table>
+                </div>
+            </DragDropContext>
+        </React.Fragment>
+    );
 
-    state = {
-        schedule: null,
-    };
+    function handleOnDragEnd (result) {
+        console.log(result);
+        if (!result.destination) return;
 
-    constructor() {
-        super();
-        const numbers = [1, 2, 3, 4, 5, 6, 7];
-        // this.state.schedule = []
-        // for (var i = 0; i < 4; i++) {
-        //     const listItems = numbers.map((number) =>
-        //         <td className="day-cell">
-        //             <div className="calendar-space">{number}</div>
-        //         </td>
-        //     );
-        //     this.state.schedule.push(<tr>{listItems}</tr>);
-        // }
-
-        this.generateUpcomingDays();
+        const items = Array.from(oldOrder);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        newOrder(items);
     }
 
-    componentDidMount() {
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <DragDropContext>
-                <div className="calendar-header">:)</div>
-                    <div className="upcoming-body">
-                        <table className="upcoming" cellPadding="0">
-                            <tbody>
-                                {this.state.schedule}
-                            </tbody>
-                        </table>
-                    </div>
-                </DragDropContext>
-            </React.Fragment>
-        );
-    }
-
-    generateUpcomingDays = () => {
+    function generateUpcomingDays () {
         var date = new Date();
         var beginning = new Date();
         beginning.setDate(1);
@@ -67,17 +54,17 @@ class Upcoming extends Component {
         var monthContent = [];
     
         var daysInMonth =
-          date.getMonth() === 1 && this.isLeapYear(date.getFullYear())
+          date.getMonth() === 1 && isLeapYear(date.getFullYear())
             ? 29
             : MONTHLENGTHS[date.getMonth()];
     
-        this.state.schedule = [];
+        var schedule = [];
         for (var w = 0; w < 4; w++) {
           var weekContent = [];
           for (var i = 0; i < 7; i++) {
             if (day <= 0) {
               weekContent.push(
-                <td className="day-cell">
+                <td className="day-cell" key={day}>
                     <div className="calendar-space">
                         {day}
                         <button className="add-card-button"> + </button>
@@ -89,36 +76,40 @@ class Upcoming extends Component {
                 day = 1;
               }
               weekContent.push(
-                <td className="day-cell">
+                <td className="day-cell" key={day}>
                     <div className={"calendar-space" + ((date.getDate() === day) ? " calendar-space-today" : "")}>
                         {day}
                         <button className="add-card-button"> + </button>
-                        <Droppable droppableId="calendar-space-content">
+                        <div>
+                        <Droppable droppableId={"calendar-space-content-" + day + "-" + i}>
                             {(provided) => (
-                                <ul className="calendar-space-content" {...provided.droppableProps} ref={provided.innerRef}>
-                                    {[1, 2, 3].map((i) => (
-                                        <Draggable key={"" + day + "-" + i} draggableId={"" + day + "-" + i} index={i}>
+                                <ul className={"calendar-space-content" + day + "-" + i} {...provided.droppableProps} ref={provided.innerRef}>
+                                    {oldOrder.map((ind) => {
+                                        return (<Draggable key={"" + day + "-" + ind} draggableId={"" + day + "-" + ind} index={ind - 1}>
                                             {(provided) => (
                                                 <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                    item {i}
+                                                    item {ind}
                                                 </li>
                                             )}
-                                        </Draggable>
-                                    ))}
+                                        </Draggable>);
+                                    })}
                                 </ul>
                             )}
                         </Droppable>
+                        </div>
                     </div>
                 </td>);
             }
             day++;
           }
     
-          this.state.schedule.push(<tr>{weekContent}</tr>);
+          schedule.push(<tr key={w}>{weekContent}</tr>);
         }
+
+        return schedule;
     }
 
-    isLeapYear = (year) => {
+    function isLeapYear (year) {
         return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
     };
 }
